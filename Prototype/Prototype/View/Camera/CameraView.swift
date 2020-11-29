ec21a472e630d0e01726c8f08cac71461392727c
Mateus Ferreira <mateusmais@icloud.com>
2020-11-29 17:22:49 -0300

    Add LauchScreen, Logo, Scare Face;

---
+++ Prototype/Prototype/View/Camera/CameraView.swift
@@ -0,0 +1,35 @@
+//
+//  CameraView.swift
+//  Prototype
+//
+//  Created by Mateus Rodrigues Santos on 24/11/20.
+//
+
+import UIKit
+
+class CameraView:UIView{
+    
+    //Variables
+    
+    //UI components
+    
+    init() {
+        super.init(frame: .zero)
+        self.isUserInteractionEnabled = true
+        self.setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CameraView:ViewCodable{
+    func setupViewHierarchy() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+}
