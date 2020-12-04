e245ed5f18df7c8220909688d5b7d4fbbfff8f49
Mateus Ferreira <mateusmais@icloud.com>
2020-12-03 22:31:27 -0300

    Import new button and Extension String

---
+++ Prototype/Prototype/Extensions/String.swift
@@ -0,0 +1,22 @@
+//
+//  String.swift
+//  Smiler
+//
+//  Created by Mateus Augusto M Ferreira on 03/12/20.
+//
+
+import Foundation
+
+extension String {
+    func applyPatternOnNumbers(pattern: String, replacmentCharacter: Character) -> String {
+        var pureNumber = self.replacingOccurrences( of: "[^0-9]", with: "", options: .regularExpression)
+        for index in 0 ..< pattern.count {
+            guard index < pureNumber.count else { return pureNumber }
+            let stringIndex = pattern.index(self.startIndex, offsetBy: index)
+            let patternCharacter = pattern[stringIndex]
+            guard patternCharacter != replacmentCharacter else { continue }
+            pureNumber.insert(patternCharacter, at: stringIndex)
+        }
+        return pureNumber
+    }
+}
